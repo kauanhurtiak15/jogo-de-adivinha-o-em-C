01c50ad1cdbc9e8cfe0c303d2991e49023def702
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-09-12 14:32:39 +0000

    em andamento

--- jogoAdivinha.c
+++ jogoAdivinha.c
@@ -2,9 +2,9 @@
 int main(){
 
     //imprime o cabeçalho do jogo.
-    printf("****************************************\n");
-    printf("* Bem vindo ao nosso jogo de adivinhação\n");
-    printf("****************************************\n");
+    printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("******************************************\n");
 
     int numeroSecreto = 50;
 
